fe2df9eaa031b86cb65ffcb5d6e6838ba78d1394
sunanda-pachuru <pachurusunanda40@gmail.com>
2025-10-02 16:26:17 +0530

    vfbmgklmb

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,33 +1,28 @@
 terraform {
-  required_version = ">= 1.0.0"
-
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
     storage_account_name = "tfstatestorageacc9000"
     container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-    # Optional: use environment variables for security in CI
-    # subscription_id      = var.subscription_id
-    # tenant_id            = var.tenant_id
+    key                  = "healthcare-app.tfstate"
   }
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~>3.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
+      version = "~>2.0"
     }
   }
 }
 
+provider "azurerm" {
+  features {}
+  
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
