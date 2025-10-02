3c8344c9fc6aa4bcdfed236ec728da4219885226
sunanda-pachuru <pachurusunanda40@gmail.com>
2025-10-02 16:56:36 +0530

    bvxfcgh

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -27,31 +27,3 @@ variable "environment" {
   type        = string
   default     = "dev"
 }
-
-variable "subscription_id" {
-  description = "Azure Subscription ID"
-  type        = string
-  sensitive   = true
-  default     = ""
-}
-
-variable "client_id" {
-  description = "Azure Client ID"
-  type        = string
-  sensitive   = true
-  default     = ""
-}
-
-variable "client_secret" {
-  description = "Azure Client Secret"
-  type        = string
-  sensitive   = true
-  default     = ""
-}
-
-variable "tenant_id" {
-  description = "Azure Tenant ID"
-  type        = string
-  sensitive   = true
-  default     = ""
-}
