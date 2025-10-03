c786c2278377b858089f03550a63deb61faba990
sunanda-pachuru <pachurusunanda40@gmail.com>
2025-10-03 15:01:18 +0530

    deleted

--- healthcare-app/terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-healthapp01"
-    storage_account_name = "sthealthapp01"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
