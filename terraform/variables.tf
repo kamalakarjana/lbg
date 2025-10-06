f941c7bc14a247222c0e18cb871921d2c9cef640
sunanda-pachuru <pachurusunanda40@gmail.com>
2025-10-06 13:51:50 +0530

    5.14

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -37,5 +37,5 @@ variable "tenant_id" {
 variable "cluster_version" {
   description = "Kubernetes version for AKS cluster"
   type        = string
-  default     = "1.26.7"
+  default     = "1.33.3"
 }
