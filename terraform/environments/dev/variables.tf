2b802e35bae0193ca6cb4540a5cc57aa34d62538
EC2 Default User <ec2-user@ip-172-31-0-133.ap-south-1.compute.internal>
2025-10-07 05:40:08 +0000

    1.38

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -71,3 +71,9 @@ variable "tags" {
     Project     = "healthcare"
   }
 }
+
+variable "vnet_address_space" {
+  description = "Virtual network address space"
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
